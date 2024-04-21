d3c4c3e0b6eeb919bebafd02f47a32ddf2ea86e6
nwitzig <nwitzig@student.ethz.ch>
2024-04-21 11:11:59 +0200

    final commit

--- reliable.c
+++ reliable.c
@@ -22,30 +22,82 @@ struct reliable_state {
     conn_t *c;			/* This is the connection object */
 
     /* Add your own data fields below this */
-    // ...
+
+    int send_sliding_window_start;
+    int send_max_window_size;
+    int send_next_not_alloc;
+    int send_eof;
+
+    int rec_sliding_window_start;
+    int rec_window_size;
+    int rec_max_window_size;
+    int rec_eof;
+    int rec_ackno;
+
+    long timeout;
+
     buffer_t* send_buffer;
-    // ...
     buffer_t* rec_buffer;
-    // ...
-    // fields added
-    int window;			/* # of unacknowledged packets in flight */
-    int timer;			/* How often rel_timer called in milliseconds */
-    int timeout;			/* Retransmission timeout in milliseconds */
-    
-    //sequence numbers to keep track off
-    uint32_t recv_ackno; // which ackno has been received
-    uint32_t send_ackno; // which seqno is the next ackno
 
-    uint32_t next_output_seqno; //next sequence number to output
-    uint32_t next_input_seqno; //next sequence number for a package created from input
-    // flags to keep track of the conditions to close the session
-    int recv_eof;
-    int read_eof;
-    int all_packs_acked;
-    int all_output_written;
 };
-
 rel_t *rel_list;
+
+
+packet_t* rel_make_data_pkt(uint16_t n, uint32_t seqno, char data[500], uint32_t ackno){
+    // creates a data packet from the params
+    packet_t *pkt = xmalloc (sizeof (packet_t));
+    pkt->ackno = htonl(ackno);
+    pkt->cksum = htons(0);
+    pkt->len = htons(n + 12);
+    pkt->seqno = htonl(seqno);
+    memcpy(pkt->data, data, 500);
+    pkt->cksum = cksum(pkt, n + 12);
+    return pkt;
+}
+
+packet_t* rel_make_eof_pkt(uint32_t seqno, uint32_t ackno){
+    // creates a data packet from the params
+    packet_t *pkt = xmalloc (sizeof (packet_t));
+    pkt->ackno = htonl(ackno);
+    pkt->cksum = htons(0);
+    pkt->len = htons(12);
+    pkt->seqno = htonl(seqno);
+    pkt->cksum = cksum(pkt, 12);
+    return pkt;
+}
+
+packet_t* rel_make_ack_pkt(uint32_t ackno){
+    // creates a ack packet from the params
+    packet_t *pkt = xmalloc (sizeof (packet_t));
+    pkt->ackno = htonl(ackno);
+    pkt->cksum = htons(0);
+    pkt->len = htons(8);
+    pkt->cksum = cksum(pkt, 8);
+    return pkt;
+}
+
+void rel_resend_pkts(rel_t* s){
+    // Now in milliseconds
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    long now_ms = now.tv_sec * 1000 + now.tv_usec / 1000;
+
+    buffer_node_t* node = buffer_get_first(s->send_buffer);
+    while(node != NULL){
+        if(node->last_retransmit + s->timeout < now_ms){
+            node->packet.ackno = htonl(s->rec_ackno);
+            node->packet.cksum = htons(0);
+            node->packet.cksum = cksum(&node->packet, ntohs(node->packet.len));
+            conn_sendpkt(s->c, &node->packet, ntohs(node->packet.len));
+            node->last_retransmit = now_ms;
+            return;
+            
+        }
+        node = node->next;
+    }
+}
+
+
 
 /* Creates a new reliable protocol session, returns NULL on failure.
 * ss is always NULL */
@@ -73,32 +125,26 @@ const struct config_common *cc)
     rel_list->prev = &r->next;
     rel_list = r;
 
-    /* Do any other initialization you need here... */
-    // ...
+    /* Do any other initialization you need here */
+
+    r->send_max_window_size = cc->window;
+    r->send_next_not_alloc = 1;
+    r->send_sliding_window_start = 1;
+    r->send_eof = 0;
+
+    r->rec_max_window_size = cc->window;
+    r->rec_sliding_window_start = 1;
+    r->rec_window_size = 0;
+    r->rec_eof = 0;
+    r->rec_ackno = 1;
+
+    r->timeout = (long) cc->timeout;
+
     r->send_buffer = xmalloc(sizeof(buffer_t));
     r->send_buffer->head = NULL;
     // ...
     r->rec_buffer = xmalloc(sizeof(buffer_t));
     r->rec_buffer->head = NULL;
-    // ...
-    // initialize added fields
-    r->window = cc->window;
-    r->timer = cc->timer;
-    r->timeout = cc->timeout;
-    
-    r->recv_ackno = 1;
-    r->send_ackno = 1;
-
-    r->next_output_seqno = 1;
-    r->next_input_seqno = 1;
-
-    //init flags to 0
-    r->all_output_written = 1;
-    r->all_packs_acked = 1;
-    r->read_eof = 0;
-    r->recv_eof = 0;
-
-    fprintf(stderr, "reliable protocol session created \n");
 
     return r;
 }
@@ -106,9 +152,8 @@ const struct config_common *cc)
 void
 rel_destroy (rel_t *r)
 {
-    if (r->next) {
+    if (r->next)
         r->next->prev = r->prev;
-    }
     *r->prev = r->next;
     conn_destroy (r->c);
 
@@ -118,228 +163,168 @@ rel_destroy (rel_t *r)
     buffer_clear(r->rec_buffer);
     free(r->rec_buffer);
     // ...
-    
-
-    fprintf(stderr, "reliable protocol session destoyed \n");
-
-}
-int check_rel_destory(rel_t *r){
-    if(r->all_output_written && r->all_packs_acked && r->read_eof && r->recv_eof){
-        rel_destroy(r);
-        return 1;
-    }
-    return 0;
+    free(r);
 }
 
-void update_send_ackno(rel_t * r){
-    // function to find the next in order sequence number
 
-    buffer_node_t *current = r->rec_buffer->head;
-    if(current == NULL){
-        // buffer is empty
+void
+rel_recvpkt (rel_t *r, packet_t *pkt, size_t n)
+{
+    // translate from networkt to host
+    uint16_t checksum = ntohs(pkt->cksum);
+    size_t pkt_size = ntohs(pkt->len);
+    size_t ackno = ntohl(pkt->ackno);
+
+    // reset cksum
+    pkt->cksum = htons(0);
+
+    // check for corruption
+    if(ntohs(cksum(pkt, n)) != checksum || n != pkt_size){
         return;
     }
-    while(current->next != NULL && ntohl(current->packet.seqno) == ntohl(current->next->packet.seqno)-1){
-        current = current->next;
-    }
-    r->send_ackno = ntohl(current->packet.seqno) + 1;
-    fprintf(stderr, "next ackno to send calculated: %d \n", r->send_ackno);
-}
 
-void send_ack(rel_t *r){
-    packet_t ack_pkt;
-    memset(&ack_pkt, 0, sizeof(ack_pkt));
-    ack_pkt.ackno = htonl(r->send_ackno); 
-    ack_pkt.len = htons(8); // ACK packet size
-    ack_pkt.cksum = 0;
-    ack_pkt.cksum = cksum(&ack_pkt, 8);
-
-    conn_sendpkt(r->c, &ack_pkt, 8);
-    fprintf(stderr, "Sent an ack with ackno %d \n", ntohl(ack_pkt.ackno));
-}
-
-// n is the expected length of pkt
-void rel_recvpkt(rel_t *r, packet_t *pkt, size_t n) {
-    
-    if(n != ntohl(pkt->len)){
-        fprintf(stderr, "packet len corrupted \n");
-        return;
-    }
-    
-    if (n < 8 || n > 512) { 
-        fprintf(stderr, "invalid packet length \n");
-        return; // Packet is corrupted or incomplete
-    }
-
-    // checksum
-    uint16_t pkt_cksum = pkt->cksum;
-    pkt->cksum = 0;
-    if (cksum(pkt, n) != pkt_cksum) {
-        fprintf(stderr, "Checksum did not match \n");
-        return; // Checksum does not match, packet is corrupted
-    }
-
-    // ack packets
-    if (n == 8) {
-        
-        fprintf(stderr, "Received an ack with ackno %d\n", ntohl(pkt->ackno));
-        fprintf(stderr, "Ackno received so far %d\n", r->recv_ackno);
-        
-        //Check if the sliding window has moved
-        if(ntohl(pkt->ackno) > r->recv_ackno){
-            fprintf(stderr, "Sliding window has changed \n");
-            r->recv_ackno =  ntohl(pkt->ackno);
+    // ack packet
+    if(n == 8){
+        if(buffer_remove(r->send_buffer, ackno) > 0){
             rel_read(r);
-            buffer_remove(r->send_buffer, r->recv_ackno);
-            fprintf(stderr, "removed packets up to %d from buffer \n", r->recv_ackno);
-        }
-
-        // check if send buffer is now empty
-        if(r->send_buffer->head == NULL){
-            fprintf(stderr, "send buffer is now empty \n");
-            r->all_packs_acked = 1;
-            return;
         }
         return;
     }
 
-    // received an eof
-    if(n == 12){
-        fprintf(stderr, "received EOF\n");
-        r->recv_eof = 1;
-        //not sure if we need to ack eofs
-        send_ack(r);
+    // data packet
+    if(n >= 12 && n <= 512){
+        //buffer_remove(r->send_buffer, ackno);
+        uint32_t seqno = ntohl(pkt->seqno);
+        //fprintf(stderr, "packet arrived : %i\n", seqno);
+
+        // check whether packet outside sliding window;
+        if(r->rec_sliding_window_start + r->rec_max_window_size < seqno){
+            fprintf(stderr, "packet outside sliding window : %i, %i\n", seqno, r->rec_ackno);
+            return;
+        }
+        if(r->rec_ackno > seqno){
+            // send ack
+            packet_t* ack_pkt = rel_make_ack_pkt(r->rec_ackno);
+            conn_sendpkt(r->c, ack_pkt, 8);
+            return;
+        }
+
+        // eof packet
+        if(n == 12){
+            //fprintf(stderr, "eof packet : %i\n", seqno);
+            r->rec_eof = 1;
+        }
+
+        // insert new packet
+        buffer_insert(r->rec_buffer, pkt, 0);
+
+        // update ackno
+        buffer_node_t* node = buffer_get_first(r->rec_buffer);
+        while(node != NULL){
+            if(ntohl(node->packet.seqno) == r->rec_ackno){
+                r->rec_ackno = ntohl(node->packet.seqno) + 1;
+            }
+            node = node->next;
+        }
+
+        // send ack
+        packet_t* ack_pkt = rel_make_ack_pkt(r->rec_ackno);
+        conn_sendpkt(r->c, ack_pkt, 8);
+
+        // try to output the received packet
+        rel_output(r);
+
+        return;
+    }
+}
+
+
+void
+rel_read (rel_t *r)
+{
+    // return if slidingwindow is at max size or there is already a small packet in the queue
+    if(buffer_size(r->send_buffer) >= r->send_max_window_size || r->send_eof == 1){
         return;
     }
 
-    // buffer does not contain the data
-    if (!buffer_contains(r->rec_buffer, ntohl(pkt->seqno))) {
-        // insert packet into buffer
-        struct timeval now;
-        gettimeofday(&now, NULL);
-        long now_ms = now.tv_sec * 1000 + now.tv_usec / 1000;
+    // Now in milliseconds
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    long now_ms = now.tv_sec * 1000 + now.tv_usec / 1000;
 
-        buffer_insert(r->rec_buffer, pkt, now_ms);
-        r->all_output_written = 0;
-        update_send_ackno(r);
-        fprintf(stderr, "Inserted packet into buffer\n");
-        
-    }else{
-        fprintf(stderr, "received a packet that was already in buffer\n");
-    }
-    send_ack(r);
-    
-}
+    char data[500];
+    int data_len = conn_input(r->c, data, 500);
+    while(data_len > 0 && buffer_size(r->send_buffer) < r->send_max_window_size){
 
-
-void rel_read(rel_t *r) {
-    // Check if the send window is full
-    int data_len;
-    do{
-        if ((r->next_input_seqno - r->recv_ackno) >= r->window) {
-            fprintf(stderr, "Max window is: %d, seqno of next packet is: %d, received in order ack up to packet: %d \n", r->window, r->next_input_seqno, (r->recv_ackno-1));
-            return; // Window is full, cannot send more data yet
-        }
-
-    // read data packet
-        char data[500];
-        data_len = conn_input(r->c, data, sizeof(data));
-        if (data_len <= 0) {
-            r->read_eof = 1;
-            fprintf(stderr, "no data read or EOF/error\n");
-            return; // No data read or EOF/error
-        }
-    
-    // send data packet
-        packet_t pkt;
-        void * pkt_pointer = memset(&pkt, 0, sizeof(pkt));
-        pkt.seqno = htonl(r->next_input_seqno); // Next sequence number
-        pkt.cksum = 0;
-        pkt.len = htons(12 + data_len);
-        memcpy(pkt.data, data, data_len);
-        pkt.cksum = cksum(&pkt, 12 + data_len);
-
-        if (conn_sendpkt(r->c, &pkt, 12 + data_len) > 0) {
-            // Successfully sent, insert into send buffer for potential retransmission
-            r->next_input_seqno++;
-            struct timeval now;
-            gettimeofday(&now, NULL);
-            long now_ms = now.tv_sec * 1000 + now.tv_usec / 1000;
-            buffer_insert(r->send_buffer, &pkt, now_ms);
-            r->all_packs_acked = 0;
-        }else{
-            free(pkt_pointer);
-        }
-        
-    }while(data_len > 0);
-
-    return;
-}
-
-
-void rel_output(rel_t *r) {
-    // fprintf(stderr, "rel_output called\n");
-    buffer_node_t *node = buffer_get_first(r->rec_buffer);
-    while (node != NULL) {
-        //ensure the sequence number is correct
-        if(ntohl(node->packet.seqno) != r->next_output_seqno){
-            break;
-        }
-        // data length excluding header
-        size_t data_len = ntohs(node->packet.len) - 12;
-        if (conn_bufspace(r->c) < data_len) {
-            // Not enough space in output buffer, exit to avoid partial writes
+        packet_t* pkt = rel_make_data_pkt(data_len, r->send_next_not_alloc, data, r->rec_ackno);
+        buffer_insert(r->send_buffer, pkt, now_ms);
+        r->send_next_not_alloc++;
+        conn_sendpkt(r->c, pkt, ntohs(pkt->len));
+        if(buffer_size(r->send_buffer) >= r->send_max_window_size){
             return;
         }
-        conn_output(r->c, node->packet.data, data_len);
-        // update next seqno to output
-        r->next_output_seqno++;
-        // Move to next packet after successful write
+        data_len = conn_input(r->c, data, 500);
+    }
+    if(data_len == -1){
+        packet_t* pkt = rel_make_eof_pkt(r->send_next_not_alloc, r->rec_ackno);
+        buffer_insert(r->send_buffer, pkt, now_ms);
+        conn_sendpkt(r->c, pkt, 12);
+        r->send_next_not_alloc++;
+        r->send_eof = 1;
+    }
+
+}
+
+void
+rel_output (rel_t *r)
+{
+    uint16_t space = (uint16_t)conn_bufspace(r->c);
+    buffer_node_t* node = buffer_get_first(r->rec_buffer);
+    while(node != NULL){
+        // check whether there is enough space in the output buffer
+        if(space < ntohs(node->packet.len) - 12){
+            return;
+        }
+        // check if all previous packets have arrived
+        if(ntohl(node->packet.seqno) >= r->rec_ackno){
+            return;
+        }
+        conn_output(r->c, node->packet.data, ntohs(node->packet.len) - 12);
+        space = (uint16_t)conn_bufspace(r->c);
+        r->rec_sliding_window_start = ntohl(node->packet.seqno);
         buffer_remove_first(r->rec_buffer);
-        node = buffer_get_first(r->rec_buffer);
+        node = node->next;
     }
-    if(node == NULL){
-        //fprintf(stderr, "all data output written\n");
-        r->all_output_written = 1;
-    }
-
 }
 
+void
+rel_timer ()
+{
+    // Go over all reliable senders, and have them send out
+    // all packets whose timer has expired
+    rel_t *current = rel_list;
+    rel_t *dest;
+    while (current != NULL) {
 
+        rel_read(current);
 
-void rel_timer() {
-
-    for (rel_t *current = rel_list; current != NULL; current = current->next) {
-        
-        //fprintf(stderr, "Received EOF %d,Read EOF %d, all packets acked %d, all output written %d \n", current->recv_eof, current->read_eof, current->all_packs_acked, current->all_output_written);
-        if(check_rel_destory(current)){
-            continue;
-        }
-
-        //call output
+        // try outputing data
         rel_output(current);
 
-        // Check and retransmit any packets that have timed out
-        struct timeval now;
-        gettimeofday(&now, NULL);
-        long now_ms = now.tv_sec * 1000 + now.tv_usec / 1000;
-        
-        for (buffer_node_t *node = buffer_get_first(current->send_buffer);
-             node != NULL; node = node->next) {
-            if (now_ms - node->last_retransmit > current->timeout) {
-                // Retransmit packet
-                node->packet.cksum = 0; // Reset checksum
-                node->packet.cksum = cksum(&node->packet, ntohs(node->packet.len));
-                conn_sendpkt(current->c, &node->packet, ntohs(node->packet.len));
-                node->last_retransmit = now_ms;
-                fprintf(stderr,"packet has been resent \n");
+        // resend packets whose timout has been triggered
+        rel_resend_pkts(current);
+
+        // check whether the connection can be destroyed
+        dest = current;
+        current = current->next;
+        if(dest->send_eof == 1 && buffer_size(dest->send_buffer) <= 1 ){
+            if(dest->rec_eof == 1 && buffer_size(dest->rec_buffer) == 0){
+                fprintf(stderr, "connection destroyed\n");
+                rel_destroy(dest);
             }
-             }
-
+        }
     }
+
+    // if s->send_eof == 1 and buffer_size(s->send_buffer) == 0 -> out eof completed
+
 }
-
-
-
-
-
